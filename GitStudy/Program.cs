eb014ad5e4094e24b53acee6cd40db3053cd0ca0
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-03-20 15:24:26 +0900

    Update Program.cs

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -10,7 +10,7 @@
             Console.WriteLine("입력하신 이름은 {0} 입니다", input);
             Console.WriteLine("Add Test");
             Console.WriteLine("쓰잘데기 없는 경우");
-            Console.WriteLine("")
+            Console.WriteLine("쓰잘데기없는 기능2");
 
 
         }
